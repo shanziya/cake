389e39c5dbfeeaed67032355ebedb0de217c6bb4
shanziya <shanziyashanzy@gmail.com>
2022-02-17 20:01:50 +0530

    cake added

---
+++ cake/cake_shop/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class category(models.Model):
+    name=models.CharField(max_length=250,unique=True)
+    slug=models.SlugField(max_length=250,unique=True)
+    img=models.ImageField(upload_to="category",blank=True)
+
+    class Meta:
+        ordering=('name',)
+        verbose_name='category'
+        verbose_name_plural='categories'
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+class Product(models.Model):
+    name=models.CharField(max_length=250,unique=True)
+    slug=models.SlugField(max_length=250,unique=True)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    description=models.TextField(blank=True)
+    img=models.ImageField(upload_to='product',blank=True)
+    class Meta:
+        ordering=('name',)
+        verbose_name='product'
+        verbose_name_plural='products'
+
+    def __str__(self):
+        return '{}'.format(self.name)
---
+++ cake/cake_shop/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render,get_object_or_404,redirect
+# from . models import category,Product
+from django.contrib.auth.models import User
+from django.contrib import messages,auth
+from django.contrib.auth.forms import AuthenticationForm
+# Create your views here.
+
+def cakeprodct(request):
+    return render(request,"category.html")
+
+def register(request):
+    if request.method == "POST":
+        customer_name = request.POST['customer_name']
+        address = request.POST['address']
+        phone_number = request.POST['phone_number']
+        district = request.POST['district']
+        password = request.POST['password']
+
+        myuser = User.objects.create_user(customer_name,password)
+        myuser.address = address
+        myuser.phone_number = phone_number
+        myuser.district = district
+
+        myuser.save()
+
+        messages.success(request,"your account has been successfully created")
+        return redirect('login')
+
+    return render(request,'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data= request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('cake_order_page')
+    else:
+        form = AuthenticationForm()
+
+    context = {'form': form}
+    return render(request,'login.html',context)
+
+def cake_order_page(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data= request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('cake_list_page')
+    else:
+        form = AuthenticationForm()
+        context = {'form': form}
+    return render(request,'cake_order_page.html',context)
+
+def cake_list_page(request):
+    return render(request, 'cake_list_page.html')
+
+
